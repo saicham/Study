4cdb14b30a2f94bba873fc5e1ec99ae74e17c744
saicham <83047799+saicham@users.noreply.github.com>
2023-06-14 09:40:32 +0900

    Delete Leeji directory

--- Leeji/module_test.py
+++
@@ -1,8 +0,0 @@
-def circle(pi, radius):
-    print("pi = ", pi)
-    print("반지름 = ", radius)
-    return pi * radius ** 2
-
-def calculate_volume(length, width, depth):
-    return length * width * depth
-
